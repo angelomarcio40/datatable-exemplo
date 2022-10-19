4b415af512491281c8d351e5ce2f4c2fe0994303
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2022-10-18 22:00:38 -0300

    Exemplo loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,15 @@ $(document).ready(function () {
   // exemplo de mascara de CEP
   // $('#cep').inputmask('99999-999')
 
+  // Após o carregamento completo da página o preloader é fechado
+  FechaLoader();
 
 });
 
 // Função que add usuários
 const addUser = () => {
+  // Ao clicar no loader abre
+  AbreLoader();
 
   // Valida se o nome foi preenchido - usando JQUERY
   // let nome = $('#nome').val()
@@ -35,14 +39,14 @@ const addUser = () => {
   let dados = new FormData($("#form-usuarios")[0]);
 
   // envio e recebimento de dados
-  const result = fetch('backend/addUser.php',{
+  const result = fetch('backend/addUser.php', {
     method: 'POST',
     body: dados
   })
-  .then((response)=>response.json())
-  .then((result)=>{
-    // Aqui é tratado o retorno ao front
-    
+    .then((response) => response.json())
+    .then((result) => {
+      // Aqui é tratado o retorno ao front
+
       Swal.fire({
         title: 'Atenção',
         text: result.mensagem,
@@ -55,36 +59,36 @@ const addUser = () => {
 
       // recarregar a tabela apos inserir o usuario
       result.retorno == 'ok' ? listUser() : ''
-    
-    
-  })
+
+
+    })
 };
 // Final da função de add usuário
 
 
 // Função que lista os usuários cadastrados
-const listUser = () =>{
+const listUser = () => {
 
-  const result = fetch('backend/listUser.php',{
+  const result = fetch('backend/listUser.php', {
     method: 'POST',
     body: ''
   })
-  .then((response)=>response.json())
-  .then((result)=>{
-    // Aqui é tratado o retorno ao front
-    let datahora = moment().format('DD/MM/YY HH:mm')
+    .then((response) => response.json())
+    .then((result) => {
+      // Aqui é tratado o retorno ao front
+      let datahora = moment().format('DD/MM/YY HH:mm')
 
-    $('#horario-atualizado').html(datahora)
+      $('#horario-atualizado').html(datahora)
 
-     
-    // destroi a tabela que foi iniciada
-    $("#tabela").dataTable().fnDestroy()
 
-    // limpa os dados da tabela
-    $('#tabela-dados').html('')
-    
-    // Funcao que irá montar as linhas da tabela, o map é um tipo de laço (for)
-    result.map(user=>{
+      // destroi a tabela que foi iniciada
+      $("#tabela").dataTable().fnDestroy()
+
+      // limpa os dados da tabela
+      $('#tabela-dados').html('')
+
+      // Funcao que irá montar as linhas da tabela, o map é um tipo de laço (for)
+      result.map(user => {
         $('#tabela-dados').append(`
           <tr>
             <td>${user.nome}</td>
@@ -101,25 +105,25 @@ const listUser = () =>{
             </td> 
           </tr>    
         `)
+      })
+
+
+      // CSS dinâmico de botão para sim e não
+      // <button type="button" class="btn btn-sm btn-${user.ativo==1 ? 'success' : 'danger'}">${user.ativo==1 ? 'Sim' : 'Não'}</button>
+
+
+
+      // inicia a datatable
+      $("#tabela").DataTable({
+        language: {
+          url: "//cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json"
+        }
+      });
+
+
+
+
     })
-
-
-    // CSS dinâmico de botão para sim e não
-    // <button type="button" class="btn btn-sm btn-${user.ativo==1 ? 'success' : 'danger'}">${user.ativo==1 ? 'Sim' : 'Não'}</button>
-
-    
-
-    // inicia a datatable
-    $("#tabela").DataTable({
-      language: {
-        url: "//cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json"        
-      }
-    });
-   
-      
-    
-    
-  })
 
 
 }
@@ -137,15 +141,15 @@ const updateUserActive = (id) => {
       'Content-type': 'application/x-www-form-urlencoded'
     }
   })
-  .then((response) => response.json()) // retorna uma promise
-  .then((result) => {
+    .then((response) => response.json()) // retorna uma promise
+    .then((result) => {
       Swal.fire({
         icon: result.retorno == 'ok' ? 'success' : 'error',
         title: result.mensagem,
         showConfirmButton: false,
         timer: 2000
-      })      
-  });
+      })
+    });
 
 
 }
@@ -160,24 +164,24 @@ const deleteUser = (id) => {
       'Content-type': 'application/x-www-form-urlencoded'
     }
   })
-  .then((response) => response.json()) // retorna uma promise
-  .then((result) => {
+    .then((response) => response.json()) // retorna uma promise
+    .then((result) => {
       Swal.fire({
         icon: result.retorno == 'ok' ? 'success' : 'error',
         title: result.mensagem,
         showConfirmButton: false,
         timer: 2000
-      }) 
-      
-      // recarregar a lista de usuários
-      listUser()  
+      })
 
-  });
+      // recarregar a lista de usuários
+      listUser()
+
+    });
 
 }
 
 
-const listUserID = (id) =>{
+const listUserID = (id) => {
   // lista os dados do usuário por ID, para alteração de dados
   // aqui teria que ser implementado toda a requisicao para o backend PHP
   // o modal terá que ser exibido dentro do result
@@ -189,31 +193,40 @@ const listUserID = (id) =>{
       'Content-type': 'application/x-www-form-urlencoded'
     }
   })
-  .then((response) => response.json()) // retorna uma promise
-  .then((result) => {
+    .then((response) => response.json()) // retorna uma promise
+    .then((result) => {
 
-    // preenche os dados dentro do form de editar usuarios
-    $('#edita-nome').val(result[0].nome)
-    $('#edita-email').val(result[0].email)
-    $('#edita-telefone').val(result[0].telefone)
-    $('#edita-cpf').val(result[0].cpf)
+      // preenche os dados dentro do form de editar usuarios
+      $('#edita-nome').val(result[0].nome)
+      $('#edita-email').val(result[0].email)
+      $('#edita-telefone').val(result[0].telefone)
+      $('#edita-cpf').val(result[0].cpf)
 
-    $('#edita-telefone').inputmask('(99) 99999-9999')
-    $('#edita-cpf').inputmask('999.999.999-99')
+      $('#edita-telefone').inputmask('(99) 99999-9999')
+      $('#edita-cpf').inputmask('999.999.999-99')
 
-    // exibe o modal apos preencher os dados para edicao
-    const modalEditar = new bootstrap.Modal('#modal-editar-usuario')
-    modalEditar.show()      
+      // exibe o modal apos preencher os dados para edicao
+      const modalEditar = new bootstrap.Modal('#modal-editar-usuario')
+      modalEditar.show()
 
-  }); 
+    });
 
 }
 
-const updateUser = () =>{
+const updateUser = () => {
   // igualzinnnn o addUser
   // JS igual a função AddUser
   // PHP igual o AddUser.php
-  
+
+}
+
+const FechaLoader = () => {
+  $('.preloader').fadeOut("slow", 0);
+}
+
+const AbreLoader = () => {
+
+  $('.preloader').fadeTo("slow", 1);
 }
 
 
